4934b766cae46ef05b318d70b4d3e16a32319420
Daquon Simpson <Daquon_simpson@yahoo.com>
2018-03-04 21:23:35 -0500

    added some javascript

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('i work')
+console.log('i work');
+
+const urlParam = window.location.search.substr(1).split("=");
+
+console.log(urlParam);
+
+const playerName = urlParam[1];
+
+console.log(playerName);
+
+
+
