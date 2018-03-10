0a9dcbfb31bc096277642e534a3f0c226536d19f
Daquon Simpson <Daquon_simpson@yahoo.com>
2018-03-10 05:16:25 -0500

    update

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const urlParam = window.location.search.substr(1).split("=");
 
 console.log(urlParam);
 
-const playerName = urlParam[1];
+const Name = urlParam[1];
 
-console.log(playerName);
+console.log(Name);
+
+let p = document.createElement("p");
+
+p.appendChild(Name);
+
+const playerName = document.getElementById('player-name');
+
+playerName.appendChild(p);
+
 
 
 
